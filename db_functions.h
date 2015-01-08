30d7a1572e1f5e49c74d69c8ab6dcd5ad0639f06
ludstr <ludvig.stromberg@gmail.com>
2015-01-08 14:28:28 +0100

    Update db_functions.h

--- db_functions.h
+++ db_functions.h
@@ -7,6 +7,15 @@ typedef struct node{
   struct node *next;
 } *Node;
 
+typedef struct tree Tree;
+
+
+Tree *newNode(char *key, char *value);
+
+Tree * treeInsert (Tree * node, Tree * tree);
+
+void initTree (char *db);
+
 void readDatabase(char *db);
 
 void readline(char *dest, int n, FILE *source);
@@ -20,6 +29,8 @@ void update_entry();
 void new_entry();
 
 void delete_entry();
+
+void printTree(Tree* tree);
 
 void print_database();
 
