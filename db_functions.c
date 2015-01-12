738e005c258da0cb70c6ec99d1e33ebd7157a28e
ludstr <ludvig.stromberg@gmail.com>
2015-01-12 20:03:10 +0100

    Update db_functions.c

--- db_functions.c
+++ db_functions.c
@@ -11,38 +11,33 @@ typedef struct tree{
   struct tree *right;
 } Tree;
 
-
-// Types
+//Types
 char buffer[128];
 int found;
 int dupecheck;
 Tree * cursor;
 Tree * treeRoot = NULL;
 
-
-
 void readline(char *dest, int n, FILE *source){
+
   fgets(dest, n, source);
   int len = strlen(dest);
   if(dest[len-1] == '\n')
     dest[len-1] = '\0';
 }
 
+void printNode (Tree *node){ 
 
-
-void printNode (Tree *node){
- 
   printf("Value: %s Key: %s", node->value, node->key);
-
-
 }
 
 Tree *newNode(char *key, char *value){
+
   if(key == NULL && value == NULL){
     return NULL;
   }
-  Tree * new = (struct tree*)malloc(sizeof(struct tree));
 
+  Tree * new = (struct tree*)malloc(sizeof(struct tree));
   new->key = (char*)malloc(strlen(key) + 1);
   strcpy(new->key, key);
   new->value = (char*)malloc(strlen(value) + 1);
@@ -50,21 +45,20 @@ Tree *newNode(char *key, char *value){
   new->right = NULL;
   new->left = NULL;
   
-  // printNode(new);
   return new;
 }
 
 void treeIns(Tree ** tree, Tree * node){
+  
   if(*tree == NULL) {
-    //puts("1");
     *tree = node;
     return;
+
   }else{
     if(strcmp(node->key, (*tree)->key)  < 0){
-      //puts("2");
       treeIns(&(*tree)->left, node);
+
     }else{
-      //puts("3");
       treeIns(&(*tree)->right, node);
     }
   }
@@ -86,27 +80,22 @@ void initTree (char *db){
      
     if((key[0] != '\0') && (value[0] != '\0')){
       Tree * treeNode = newNode(key, value);
-      // printf("%p ", newTree);
       treeIns(&newTree, treeNode);
-
     }
-  }
-  
-  //printTree(newTree);
+  }  
   treeRoot = newTree;
-  //printTree(treeRoot);
-
 }
 
 void startprocedure(int choice) {
+
   if (choice == 0) {
     printf("Enter key: ");
     readline(buffer, 128, stdin);
     puts("Searching database...\n");
     found = 0;
     cursor = treeRoot;
+
   }else{
-    // new entry case
     printf("Enter key: ");
     readline(buffer, 128, stdin);
     puts("Searching database for duplicate keys...");
@@ -121,19 +110,16 @@ Tree * searchKey (Tree ** tree, char * key){
   cursor = *tree;
 
   if(cursor == NULL){
-
     return NULL;
 
-  }else{
-    
+  }else{ 
     if(strcmp(key, cursor->key) == 0){      
       
     }else if(strcmp(key, cursor->key) < 0){
       searchKey(&(cursor->left), key);
      
     }else{
-      searchKey(&(cursor->right), key);
-     
+      searchKey(&(cursor->right), key); 
     }
   }
   return cursor;
@@ -141,8 +127,6 @@ Tree * searchKey (Tree ** tree, char * key){
 
 
 void query(){
-
-  puts("QUERY");
 
   startprocedure(0);
   char * tempKey = malloc(strlen(buffer) + 1);
@@ -154,50 +138,42 @@ void query(){
     
   }else{
     puts("Key not found in database!");
-    
-
   }
+  free(tempKey);
 }
 
 void update_entry(){
 
-  puts("UPDATE");
-
   startprocedure(0);
-  /*
-  while(!found && cursor != NULL){
-    if(strcmp(buffer, cursor->key) == 0){
-      puts("Matching entry found:");
-      printf("key: %s\nvalue: %s\n\n", cursor->key, cursor->value);
-      found = 1;
-    }else{
-      cursor = cursor->next;
-    }
-  }
-  if(!found){
-    printf("Could not find an entry matching key \"%s\"!\n", buffer);
-  }else{
-    printf("Enter new value: ");
+  char * tempKey = malloc(strlen(buffer) + 1);
+  strcpy(tempKey, buffer);
+  Tree * Node = searchKey(&treeRoot, tempKey);
+  
+  if(Node != NULL){
+    printf("Enter new value to insert\n");
     readline(buffer, 128, stdin);
-    free(cursor->value);
-    cursor->value = malloc(strlen(buffer) + 1);
-    strcpy(cursor->value, buffer);
-    puts("Value inserted successfully!");
-  }
-  */
-}
+    char * tempValue = malloc(strlen(buffer) + 1);
+    strcpy(tempValue, buffer);
+    strcpy(Node->value, tempValue);
+    printf("Value updated: ");
+    printNode(Node);
+    free(tempValue);
 
+  }else{
+    puts("Key not found in database!");
+  }
+  free(tempKey);
+
+}
 
 void searchDuplicate (Tree ** tree, char * key){
 
   cursor = *tree;
   
-
   if(cursor == NULL){
     dupecheck = 0;
 
   }else{
-
     if(strcmp(key, cursor->key) == 0){
       dupecheck = 1;
 
@@ -215,89 +191,86 @@ void new_entry(){
   startprocedure(1);
   char * tempKey = malloc(strlen(buffer) + 1);
   strcpy(tempKey, buffer);
-
-  //puts("2");
   searchDuplicate(&treeRoot, tempKey);
-  //printf("%d", found);
 
   if(dupecheck == 1){
+
     printf("key \"%s\" already exists!\n", buffer);
  
   }else{
-    //char * tempKey = buffer;
-    //strcpy(tempKey, buffer);
     printf("Enter value: ");
     readline(buffer, 128, stdin);
     char * tempVal = malloc(strlen(buffer) + 1);
     strcpy(tempVal, buffer);
     printf("%s \n", tempKey);
     Tree * insertNode = newNode(tempKey, tempVal);
-    //printNode(insertNode);
     treeIns(&treeRoot, insertNode);
     
     printf("Key: %s with value: %s added to database", tempKey, tempVal);
+    free(tempVal);
+  }
+  free(tempKey); 
+}    
 
+Tree * findMinNode(Tree * tree){
+
+  Tree * cursor = tree;
+  
+  while(cursor->left != NULL){
+    cursor = cursor->left;
+  }
+  return cursor;
+}
+
+Tree * delNode(Tree * tree, char * key){
+
+  if(tree == NULL) return tree;
+
+  if(strcmp(key, tree->key) < 0){
+    tree->left = delNode(tree->left, key);
+  }
+  
+  else if(strcmp(key, tree->key) > 0){
+    tree->right = delNode(tree->right, key);
   }
 
-}    
-  //while(!found && cursor != NULL){
+  else{
+
+    if (tree->left == NULL){
+      Tree * temp = tree->right;
+      free(tree);
+      return temp;
+      
+    }else if(tree->right == NULL){
+      Tree * temp = tree->left;
+      free(tree);
+      return temp;      
+    }
     
-
-
-  
-    /*
-  while(!found && cursor != NULL){
-        if(strcmp(buffer, cursor->key) == 0){
-          printf("key \"%s\" already exists!\n", cursor->key);
-          found = 1;
-        }else{
-          cursor = cursor->next;
-        }
-      }
-      if(!found){ // Insert new node to the front of the list
-        puts("Key is unique!\n");
-        Node newNode = malloc(sizeof(struct node));
-        newNode->key = malloc(strlen(buffer) + 1);
-        strcpy(newNode->key, buffer);
-        printf("Enter value: ");
-        readline(buffer, 128, stdin);
-        newNode->value = malloc(strlen(buffer) + 1);
-        strcpy(newNode->value, buffer);
-        newNode->next = list;
-        list = newNode;
-        puts("");
-        puts("Entry inserted successfully:");
-        printf("key: %s\nvalue: %s\n", list->key, list->value);
-      }
-    */
-
+    Tree * temp = findMinNode(tree->right);
+    tree->key = temp->key;
+    tree->value = temp->value;
+    tree->right = delNode(tree->right, temp->key);    
+  }
+  return tree;
+}
 
 void delete_entry(){
 
-  puts("DELETE");
-
-  /*
-
   startprocedure(0);
-    Node prev = NULL;
-      while(!found && cursor != NULL){
-        if(strcmp(buffer, cursor->key) == 0){
-          if(prev == NULL){ // Delete first node
-            list = cursor->next;
-          }else{
-            prev->next = cursor->next;
-          }
-          found = 1;
-          printf("Deleted the following entry:\nkey: %s\nvalue: %s\n", cursor->key, cursor->value);
-        }else{
-          prev = cursor;
-          cursor = cursor->next;
-        }
-      }
-      if(!found){
-        printf("Could not find an entry matching key \"%s\"!\n", buffer);
-      }
-  */
+  char * tempKey = malloc(strlen(buffer) + 1);
+  strcpy(tempKey, buffer);
+  Tree * temp = searchKey(&treeRoot, tempKey);
+  
+  if(temp != NULL){
+    delNode(treeRoot, tempKey);
+    puts("Database entry deleted");
+
+  }else{
+    puts("Key not found in database!");
+  }
+  free(tempKey);
+  //free(temp);
 }
 
 
@@ -315,7 +288,32 @@ void printTree(Tree* tree){
 }
 
 void print_database(){
-
   printTree(treeRoot);
+
+}
+
+void freeNode(Tree * node){
+  free(node->key);
+  free(node->value);
+  free(node);
+}
+
+void cleanMemory(Tree ** tree){
+  if ((*tree)->left != NULL){
+    freeNode((*tree)->left);
+    puts("1");
+  }
+  if ((*tree)->right != NULL){
+    freeNode((*tree)->right);  
+    puts("2");
+  }
+  freeNode(*tree);
+  puts("3");
+}
+ 
+void exitProcedure(){
+
+  cleanMemory(&treeRoot);
+  puts("Memory cleared \n");
 
 }
